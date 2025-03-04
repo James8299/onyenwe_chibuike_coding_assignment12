64417a92226d4f1f880d37b5c01e8bff01607dbb
Onyenwe Chibuike <conyenwe@academic.rrc.ca>
2025-03-04 08:35:16 -0600

    second commit

--- rollup.config.js
+++ rollup.config.js
@@ -3,28 +3,33 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+const packageJson = require("./package.json");
+
 export default [
-  // Main build (ESM & CJS)
   {
     input: "src/index.ts",
     output: [
-      { file: "dist/esm/index.js", format: "esm", sourcemap: true },
-      { file: "dist/cjs/index.js", format: "cjs", sourcemap: true }
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
     ],
     plugins: [
-      resolve(), 
+      resolve(),
       commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.json"
-      })
+      typescript({ tsconfig: "./tsconfig.json" }),
     ],
-    external: ["react", "react-dom"] // Mark React as external to avoid bundling it
   },
-  
-  // Type Definitions (.d.ts)
   {
     input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()]
-  }
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
+
